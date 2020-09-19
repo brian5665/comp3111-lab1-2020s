62c3457138351ed8e6b54eb25565209f669dfa12
bri9 <53130738+bri9@users.noreply.github.com>
2020-09-19 11:09:25 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -6,6 +6,8 @@ package Lab1;
 public class Library {
 //   Add this function
 	public static void main(String arg[]) {
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
 		System.out.println("When there is a fire, commit and push.");
 		
 	}
